d8a414a9e4c77eeb518adb8a083973f88a50f510
Kim <kim.zyong@yandex.ru>
2021-07-21 17:40:33 +0300

    --

---
+++ Day 3/tmp.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+    double* a = new double[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) swap(a[j], a[j + 1]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << fixed << setprecision(2) << a[i] << " ";
+    }
+}
